6320dd0d9561518145b822502f75da16ad850cec
larkee <larkee@users.noreply.github.com>
2020-11-25 16:18:34 +1100

    refactor: use kwargs for EncryptionConfig conversion

--- google/cloud/spanner_v1/database.py
+++ google/cloud/spanner_v1/database.py
@@ -127,9 +127,7 @@ class Database(object):
         self._earliest_version_time = None
 
         if type(encryption_config) == dict:
-            self._encryption_config = EncryptionConfig(
-                kms_key_name=encryption_config["kms_key_name"]
-            )
+            self._encryption_config = EncryptionConfig(**encryption_config)
         else:
             self._encryption_config = encryption_config
 
